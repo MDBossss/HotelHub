11f1e5a427fbdef64cf85260463681e4de2b5bcd
MDBossss <ferrari020203@gmail.com>
2023-03-31 20:38:15 +0200

    added navigation 🏆

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import {Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Deals from "./pages/Deals/Deals";
+import Blog from "./pages/Blog/Blog";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
 
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/deals" element={<Deals/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
     </div>
   )
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import {RxHamburgerMenu} from "react-icons/rx";
 import {BsCurrencyDollar} from "react-icons/bs";
 import {RiEnglishInput} from "react-icons/ri";
 import { HiOutlineUserCircle } from "react-icons/hi";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -11,13 +12,13 @@ const Navbar = () => {
                 <RxHamburgerMenu/>
             </div>
             <div className="logo">
-                <h1>Hotel<span>.hub</span></h1>
+                <NavLink to="/" className="no-outline"><h1>Hotel<span>.hub</span></h1></NavLink>
             </div>
             <div className="pages">
-                <p>Last Minute Deals</p>
-                <p>Blog</p>
-                <p>About us</p>
-                <p>Contacts</p>
+                <NavLink to="/deals" className="no-outline"><p>Last Minute Deals</p></NavLink>
+                <NavLink to="/blog" className="no-outline"><p>Blog</p></NavLink>
+                <NavLink to="/about" className="no-outline"><p>About us</p></NavLink>
+                <NavLink to="/contact" className="no-outline"><p>Contacts</p></NavLink>
             </div>
         </div>
         <div className="right">
