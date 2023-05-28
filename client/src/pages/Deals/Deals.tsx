890f33d491bfa49019045e4bbcec854d56f68883
MDBossss <ferrari020203@gmail.com>
2023-05-28 16:25:37 +0200

    Update Deals.tsx

--- client/src/pages/Deals/Deals.tsx
+++ client/src/pages/Deals/Deals.tsx
@@ -32,7 +32,7 @@ const Deals = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-        fetch(import.meta.env.VITE_API_BASE_URL + "/api/offers")
+        fetch(`${import.meta.env.VITE_API_BASE_URL}/api/offers`)
         .then(response => response.json())
         .then(data => {
             setOffers(data)
